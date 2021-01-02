4902093cc90e91a5b3a8ee5f0559c39898d7db42
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2021-01-02 17:43:58 -0500

    working on tabs

--- src/content.js
+++ src/content.js
@@ -8,6 +8,8 @@ const createPage = (() => {
     header.textContent = 'The best restaraunt in town!';
     description.textContent = "Try our deluxe pizza and pasta combination - Weekends only!";
 
+    image.classList.add('image');
+    
     container.appendChild(image);
     container.appendChild(header);
     container.appendChild(description);
