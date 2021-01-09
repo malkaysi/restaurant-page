035f48cf33f742ce5fa5cf15999ba1e9a734e625
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2021-01-09 17:18:10 -0500

    creating tab array/object

---
+++ src/menuTabs.js
@@ -0,0 +1,11 @@
+const menuTabs = [
+    {
+        textContent: "This is the menu <br> Item number One is delicious <br> The second dish is a salad app <br>The third dish is full of cheese",
+    },
+    {
+        textContent: "Test test test"
+    },
+    {
+        textContent: "23 menu item for tab 3"
+    }
+]
