cf3ae3e0e45474d8079b3caf108d77012a7219df
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2021-01-09 20:11:38 -0500

    creating menu tabs

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 import createPage from "/src/content.js";
-/* import {populateTabOne, tabOneText} from "./tabOne.js"
-import populateTabTwo from "./tabTwo.js" */
 import clearMenu from './clearMenu.js'
+import popTab from './menuTabs.js'
 
 
 const menu = document.querySelector('#menu');
-const tabs = document.querySelectorAll('button')
-/* const tabOne = document.getElementById('tabOneContent');
-const tabTwo = document.getElementById('tabTwoContent'); */
+const tabs = document.querySelectorAll('div.button')
+let tabValue;
+
 
 
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
-        
-        value = tab.value;
-        console.log(value)
+        tabValue = parseInt(tab.id)
+        popTab(tabValue);
     })
 })
 
-
-
-/* 
-tabOne.addEventListener('click', () => {
-    clearMenu();
-    populateTabOne();
-});
-
-tabTwo.addEventListener('click', () => {
-    clearMenu();
-    populateTabTwo();
-}); */
+export default tabValue;
--- src/menuTabs.js
+++ src/menuTabs.js
@@ -1,23 +1,29 @@
-
+import tabValue from './index.js'
 
 const menuTabs = [
     {
         dom: document.createElement('p'),
-        textContent: "This is the menu <br> Item number One is delicious <br> The second dish is a salad app <br>The third dish is full of cheese"
+        text: "This is the menu <br> Item number One is delicious <br> The second dish is a salad app <br>The third dish is full of cheese"
     },
     {
         dom: document.createElement('p'),
-        textContent: "Test test test"
+        text: "Test test test"
     },
     {
         dom: document.createElement('p'),
-        textContent: "23 menu item for tab 3"
+        text: "23 menu item for tab 3"
     }
 ]
 
-/* const popTab = (value) => {
+const popTab = (tabValue) => {
     const menu = document.querySelector('#menu');
+    let tabObj = menuTabs[tabValue];
+    let tab = document.createElement('p')
+    tab.classList.add('tabSelected')
+    tab.textContent = tabObj.text;
 
-    menu.appendChild(menuTabs[value])
+    menu.appendChild(tab)
 
-} */
+}
+
+export default popTab
